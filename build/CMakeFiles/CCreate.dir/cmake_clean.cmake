53d08b7c7db9715e9073711062d38d72739ebdd2
Tim Dowling <takitatd@gmail.com>
2022-03-06 08:09:53 +1100

    Edited gitignore

--- build/CMakeFiles/CCreate.dir/cmake_clean.cmake
+++ build/CMakeFiles/CCreate.dir/cmake_clean.cmake
@@ -2,6 +2,7 @@ file(REMOVE_RECURSE
   "CCreate"
   "CCreate.pdb"
   "CMakeFiles/CCreate.dir/src/main.cpp.o"
+  "CMakeFiles/CCreate.dir/src/main.cpp.o.d"
 )
 
 # Per-language clean rules from dependency scanning.

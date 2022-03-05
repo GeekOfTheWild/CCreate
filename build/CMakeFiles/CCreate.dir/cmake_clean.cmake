6f99771c096e090ce85d0e666ce032f437c33d1c
Geek of the Wild <thealmonddestroyer@gmail.com>
2022-03-06 08:05:32 +1100

    init

---
+++ build/CMakeFiles/CCreate.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CCreate"
+  "CCreate.pdb"
+  "CMakeFiles/CCreate.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CCreate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

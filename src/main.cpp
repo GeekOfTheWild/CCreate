73655201656e314aef2713bc8ceb500d6ef41595
GotW <65318026+GeekOfTheWild@users.noreply.github.com>
2022-03-06 12:47:39 +1100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -26,11 +26,16 @@ int main(int argc, char *argv[])
             case 'c':
                 cvalue = optarg;
                 break;
-            //case '?':
-            //    if(optopt == 'c')
-            //        fprintf(stderr, "Option -%c requires an argument.\n", optopt);
-            //    else if(isprint(optopt))
-            //        fprintf()
+            case '?':
+                if (optopt == 'c')
+                    fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+                else if (isprint (optopt))
+                    fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+                else
+                    fprintf (stderr,
+                   "Unknown option character `\\x%x'.\n",
+                   optopt);
+                    return 1;
             default:
             abort();
         }
